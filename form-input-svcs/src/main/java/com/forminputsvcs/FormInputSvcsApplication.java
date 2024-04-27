5ccdcde92f5c8cb3c32f2d1b5dcc3d911e9aa538
saurabh <skonpratiwar8@gmail.com>
2024-04-27 20:57:38 +0530

    Initial commit

---
+++ form-input-svcs/src/main/java/com/forminputsvcs/FormInputSvcsApplication.java
@@ -0,0 +1,13 @@
+package com.forminputsvcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormInputSvcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormInputSvcsApplication.class, args);
+	}
+
+}
